f98ac165c79eca0244e6c18b92d2a9ac7483df36
Elon <elon3011@gmail.com>
2023-01-07 12:14:32 -0500

    lol

--- src/commands/information/leaderboard.js
+++ src/commands/information/leaderboard.js
@@ -61,11 +61,11 @@ module.exports = {
             .setColor(EMBED_COLORS.ERROR)
 
         if (type === "Total") response = await getTotalLeaderboard(interaction.member);
-        else if (type === "Bans") response = await getBansLeaderboard(interaction.user);
-        else if (type === "Ban-Bolos") response = await getBanBolosLeaderboard(interaction.user);
-        else if (type === "Kicks") response = await getKicksLeaderboard(interaction.user);
-        else if (type === "Warns") response = await getWarnsLeaderboard(interaction.user);
-        else if (type === "Others") response = await getOthersLeaderboard(interaction.user);
+        else if (type === "Bans") response = await getBansLeaderboard(interaction.member);
+        else if (type === "Ban-Bolos") response = await getBanBolosLeaderboard(interaction.member);
+        else if (type === "Kicks") response = await getKicksLeaderboard(interaction.member);
+        else if (type === "Warns") response = await getWarnsLeaderboard(interaction.member);
+        else if (type === "Others") response = await getOthersLeaderboard(interaction.member);
         else response = { embeds: [invalidLb] };
 
         await interaction.followUp(response);
@@ -91,19 +91,19 @@ async function getTotalLeaderboard(member, client) {
     const guild = member.guild;
 
     const collector = lb
-        .map((user, i) => `**#${(i + 1).toString()}** - ${guild.members.cache.get(user._id)}  [\`${user.logs?.total.toString()}\`]`)
+        .map((user, i) => `**#${(i + 1).toString()}** - ${guild.members.cache.get(user._id)}  [\`${user.logs?.total}\`]`)
         .join("\n");
 
     const embed = new EmbedBuilder()
         .setTitle("Total Logs Leaderboard")
         .setDescription(collector)
         .setColor(EMBED_COLORS.BOT_EMBED)
-        .setFooter({ text: `Requested by ${member.tag}` });
+        .setFooter({ text: `Requested by ${member.user.tag}` });
 
     return { embeds: [embed] };
 }
 
-async function getBansLeaderboard(author) {
+async function getBansLeaderboard(member) {
     const lb = await getBansLb(10);
 
     if (lb.length === 0) {
@@ -115,20 +115,22 @@ async function getBansLeaderboard(author) {
         return { embeds: [embed] };
     }
 
+    const guild = member.guild;
+
     const collector = lb
-        .map((user, i) => `**#${(i + 1).toString()}** - ${escapeInlineCode(user.username)} [\`${user.logs?.bans.toString()}\`]`)
+        .map((user, i) => `**#${(i + 1).toString()}** - ${guild.members.cache.get(user._id)} [\`${user.logs?.bans}\`]`)
         .join("\n");
 
     const embed = new EmbedBuilder()
         .setTitle("Total Bans Leaderboard")
         .setDescription(collector)
         .setColor(EMBED_COLORS.BOT_EMBED)
-        .setFooter({ text: `Requested by ${author.tag}` });
+        .setFooter({ text: `Requested by ${member.user.tag}` });
 
     return { embeds: [embed] };
 }
 
-async function getBanBolosLeaderboard(author) {
+async function getBanBolosLeaderboard(member) {
     const lb = await getBanBolosLb(10);
 
     if (lb.length === 0) {
@@ -140,20 +142,22 @@ async function getBanBolosLeaderboard(author) {
         return { embeds: [embed] };
     }
 
+    const guild = member.guild;
+
     const collector = lb
-        .map((user, i) => `**#${(i + 1).toString()}** - ${escapeInlineCode(user.username)} [\`${user.logs?.banbolos.toString()}\`]`)
+        .map((user, i) => `**#${(i + 1).toString()}** - ${guild.members.cache.get(user._id)} [\`${user.logs?.banbolos}\`]`)
         .join("\n");
 
     const embed = new EmbedBuilder()
         .setTitle("Total Ban-Bolos Leaderboard")
         .setDescription(collector)
         .setColor(EMBED_COLORS.BOT_EMBED)
-        .setFooter({ text: `Requested by ${author.tag}` });
+        .setFooter({ text: `Requested by ${member.user.tag}` });
 
     return { embeds: [embed] };
 }
 
-async function getKicksLeaderboard(author) {
+async function getKicksLeaderboard(member) {
     const lb = await getKicksLb(10);
 
     if (lb.length === 0) {
@@ -165,20 +169,22 @@ async function getKicksLeaderboard(author) {
         return { embeds: [embed] };
     }
 
+    const guild = member.guild;
+
     const collector = lb
-        .map((user, i) => `**#${(i + 1).toString()}** - ${escapeInlineCode(user.username)} [\`${user.logs?.kicks.toString()}\`]`)
+        .map((user, i) => `**#${(i + 1).toString()}** - ${guild.members.cache.get(user._id)} [\`${user.logs?.kicks}\`]`)
         .join("\n");
 
     const embed = new EmbedBuilder()
         .setTitle("Total Kicks Leaderboard")
         .setDescription(collector)
         .setColor(EMBED_COLORS.BOT_EMBED)
-        .setFooter({ text: `Requested by ${author.tag}` });
+        .setFooter({ text: `Requested by ${member.user.tag}` });
 
     return { embeds: [embed] };
 }
 
-async function getWarnsLeaderboard(author) {
+async function getWarnsLeaderboard(member) {
     const lb = await getWarnsLb(10);
 
     if (lb.length === 0) {
@@ -190,20 +196,22 @@ async function getWarnsLeaderboard(author) {
         return { embeds: [embed] };
     }
 
+    const guild = member.guild;
+
     const collector = lb
-        .map((user, i) => `**#${(i + 1).toString()}** - ${escapeInlineCode(user.username)} [\`${user.logs?.warns.toString()}\`]`)
+        .map((user, i) => `**#${(i + 1).toString()}** - ${guild.members.cache.get(user._id)} [\`${user.logs?.warns}\`]`)
         .join("\n");
 
     const embed = new EmbedBuilder()
         .setTitle("Total Warns Leaderboard")
         .setDescription(collector)
         .setColor(EMBED_COLORS.BOT_EMBED)
-        .setFooter({ text: `Requested by ${author.tag}` });
+        .setFooter({ text: `Requested by ${member.user.tag}` });
 
     return { embeds: [embed] };
 }
 
-async function getOthersLeaderboard(author) {
+async function getOthersLeaderboard(member) {
     const lb = await getOtherLb(10);
 
     if (lb.length === 0) {
@@ -215,15 +223,17 @@ async function getOthersLeaderboard(author) {
         return { embeds: [embed] };
     }
 
+    const guild = member.guild;
+
     const collector = lb
-        .map((user, i) => `**#${(i + 1).toString()}** - ${escapeInlineCode(user.username)} [\`${user.logs?.other.toString()}\`]`)
+        .map((user, i) => `**#${(i + 1).toString()}** - ${guild.members.cache.get(user._id)} [\`${user.logs?.other}\`]`)
         .join("\n");
 
     const embed = new EmbedBuilder()
         .setTitle("Total Other Leaderboard")
         .setDescription(collector)
         .setColor(EMBED_COLORS.BOT_EMBED)
-        .setFooter({ text: `Requested by ${author.tag}` });
+        .setFooter({ text: `Requested by ${member.user.tag}` });
 
     return { embeds: [embed] };
 }
